70a5ebe9ac02d8f5af603a5aa6c9d3a4ce32bdcf
Alexandre Lacroix <alexlacroix03@gmail.com>
2026-09-10 21:11:40 +0200

    fix(sddm): custom connect screen

--- modules/sddm.nix
+++ modules/sddm.nix
@@ -1,9 +1,20 @@
 { config, lib, pkgs, ... }:
 
 {
+  environment.systemPackages = [
+    (pkgs.catppuccin-sddm.override {
+      flavor = "mocha";
+      font = "JetBrainsMono Nerd Font";
+      fontSize = "10";
+      loginBackground = true;
+    })
+  ];
+  
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
+    theme = "catppuccin-mocha-mauve";
+    package = pkgs.kdePackages.sddm;
   };
 
   services.displayManager.defaultSession = "hyprland-uwsm";
